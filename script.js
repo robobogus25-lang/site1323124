6f93a4e8862582fcfb9d9c7c22cbdd4c67632d7b
robobogus25-lang <robobogus25@gmail.com>
2025-12-06 19:15:26 +0200

    13412

--- script.js
+++ script.js
@@ -1,47 +1,86 @@
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get('id');
-
-let profile = document.getElementById('profile');
-let productsGrid = document.getElementById('user-products-grid');
-
+let productsGrid = document.getElementById('products-grid');
+let productsArray = [];
+let xhr = new XMLHttpRequest();
 let url = 'https://my-json-server.typicode.com/robobogus25-lang/site1323124';
 
-//User details request
-let userRequest = new XMLHttpRequest();
-
-userRequest.open('GET',`${url}/users/${id}`);
-userRequest.responseType = 'json'
-userRequest.onload = function() { 
-    let user = userRequest.response;
-    profile.innerHTML = `
-        <h1>${user.name}</h1>
-        <h2>${user.sirname}</h2>
-        <img class="profile-img" src="${user.photo_url}">
-        <p>Balance: ${user.balance}$</p>
-    `
-}
-
-userRequest.send();
-
-
-//User products request
-let productsRequest = new XMLHttpRequest();
-
-productsRequest.open('GET',`${url}/products?author_id=${id}`);
-productsRequest.responseType = 'json'
-productsRequest.onload = function() { 
-    let products = productsRequest.response;
+xhr.open('GET',url + '/products');
+xhr.responseType = 'json'
+xhr.onload = function() {
+    productsArray = xhr.response
     productsGrid.innerHTML = null;
-    products.forEach(p => {
-        productsGrid.innerHTML += `
-            <div class="product">
-                <h2 class='product-name'>${p.name}</h2>
-                <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
-                <p class='product-price'><b>Price: </b>${p.price}$</p>
-                <p class='product-description'><b>Description: </b>${p.description}</p>
-            </div>
+    productsArray.forEach(p => {
+        productsArray.push(p);
+        let pElem = document.createElement('div');
+        pElem.classList.add('product');
+        pElem.innerHTML = `
+            <h2 class='product-name'>${p.name}</h2>
+            <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
+            <p class='product-price'><b>Price: </b>${p.price}$</p>
+            <p class='product-description'><b>Description: </b>${p.description}</p>
+            <a href='userProfile.html?id=${p.author_id}'>Seller profile</a>
+            <button onclick="addProductToCart(${p.id})">Buy</button>
         `;
+        productsGrid.append(pElem);
     });
 }
+xhr.send();
 
-productsRequest.send();
+function addProductToCart(id) {
+    xhr.open('GET',`${url}/products/${id}`);
+    xhr.responseType = 'json'
+    xhr.onload = function() {
+
+    }
+}
+
+// CART ----------------
+
+let cartProd = document.getElementById('cart-products');
+
+let cart = [];
+if(localStorage.getItem('cart')) {
+    cart = JSON.parse(localStorage.getItem('cart'));
+    drawCartProducts();
+}
+
+
+function addProductToCart(id) {
+    let product = productsArray.find(function(p) {
+        return p.id == id;
+    })
+    cart.push(product);
+    drawCartProducts();
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    document.getElementById('cart-button').classList.add('active');
+    setTimeout(function(){
+        document.getElementById('cart-button').classList.remove('active');
+    },500);
+}
+
+function drawCartProducts() {
+    if(cart.length === 0) return cartProd.innerHTML = 'Cart is empty';
+    cartProd.innerHTML = null;
+    let sum = 0;
+    cart.forEach(function(p){
+        cartProd.innerHTML += `
+            <p><img src="${p.photo_url}"> ${p.name} |${p.price}$</p>
+            <hr>
+        `;
+        sum += p.price;
+    });
+    cartProd.innerHTML += `
+        <p>Total Price: ${sum}$</p>
+        <button onclick="buyAll()">Buy All</button>
+    `;
+}
+
+function buyAll() {
+    cart = [];
+    cartProd.innerHTML = 'Money was withdrawn from your credit card';
+    localStorage.setItem("cart", '[]');
+}
+
+function openCart() {
+    cartProd.classList.toggle('hide');
+}
